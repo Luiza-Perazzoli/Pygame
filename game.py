f00631bf257be1cb8124f826c0c07b2f1a8fc308
Luiza-Perazzoli <luiza.perazzolim@gmail.com>
2022-05-23 15:50:30 -0300

    organizei as variáveis

--- game.py
+++ game.py
@@ -1,18 +1,24 @@
 import pygame
+from random import*
 
-pygame.init()
+imagem_fundo=r"C:\Users\luiza\OneDrive\Imagens\fundo do mar.webp"
+imagem_peixe=r"C:\Users\luiza\OneDrive\Imagens\peixe2.jpg"
+imagem_alga=r"C:\Users\luiza\OneDrive\Imagens\alga.png"
 WIDTH = 700
 HEIGHT = 400
+
+pygame.init()
+
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('flappy fish')
 
 game = True
 
-image = pygame.image.load(r"C:\Users\luiza\OneDrive\Imagens\fundo do mar.webp").convert()
-image = pygame.transform.scale(image, (700, 400)) 
+image = pygame.image.load(imagem_fundo).convert()
+image = pygame.transform.scale(image, (WIDTH, HEIGHT)) 
 
 font = pygame.font.SysFont("Times", 20)
-text = font.render('pontos:', True, (255, 0, 127))
+pontuacao = font.render('pontos:', True, (255, 0, 127))
 
 while game:
     for event in pygame.event.get():
@@ -21,7 +27,7 @@ while game:
 
     window.fill((173, 216, 230))
     window.blit(image, (10, 10))
-    window.blit(text, (600, 10))
+    window.blit(pontuacao, (600, 10))
 
     pygame.display.update() 
 
